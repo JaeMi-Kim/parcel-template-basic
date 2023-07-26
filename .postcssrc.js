8d49ec44e0aabe569faf258522dd23e55e0cb256
JaeMi-Kim <true0208@naver.com>
2023-07-27 00:42:19 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,21 @@
+// ESM - 에크마스크립트모듈 (자바스크립트 표준명칭 - 브라우저에서 동작하는 계념)
+// CommonJS - ndoejs 환경에서 동작하는 계념
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// // export {
+// //   plugins:[
+// //     autoprefixer
+// //   ]
+// // }
+// module.exports = {
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins:[
+    require('autoprefixer')
+  ]
+}
